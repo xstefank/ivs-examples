990309b35ba0544eb9b8a84b9d75d46a8a76e2cd
xstefank <xstefank122@gmail.com>
2024-04-17 09:10:58 +0200

    Fix UUID generating in images

--- docker-c-webserver-example/main.c
+++ docker-c-webserver-example/main.c
@@ -6,7 +6,7 @@
 #include <netdb.h> 
 #include <arpa/inet.h>
 #include <signal.h>
-#include <uuid/uuid.h>
+#include <time.h>
 
 #define HOST "ip-api.com"
 #define WEB_PORT 8080
@@ -15,6 +15,29 @@
 
 int server_sockfd, client_sockfd;
 char *uuid;
+
+char* gen_uuid() {
+    char v[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    //3fb17ebc-bc38-4939-bc8b-74f2443281d4
+    //8 dash 4 dash 4 dash 4 dash 12
+    static char buf[37] = {0};
+
+    //gen random for all spaces because lazy
+    for(int i = 0; i < 36; ++i) {
+        buf[i] = v[rand()%16];
+    }
+
+    //put dashes in place
+    buf[8] = '-';
+    buf[13] = '-';
+    buf[18] = '-';
+    buf[23] = '-';
+
+    //needs end byte
+    buf[36] = '\0';
+
+    return buf;
+}
 
 char *get_response() {
     int sockfd;
@@ -99,10 +122,8 @@ void sig_handler(int signo)
 }
 
 int main() {
-    uuid_t binuuid;
-    uuid_generate_random(binuuid);
-    uuid = malloc(37);
-    uuid_unparse(binuuid, uuid);
+    srand(time(NULL));
+    uuid = gen_uuid();
 
     struct sockaddr_in server_addr, client_addr; 
     char client_request[BUFFER_LENGTH];
